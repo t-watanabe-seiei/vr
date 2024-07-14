9bbfd905f04e210accc554e006e47bf96cc9f900
t-watanabe-seiei <139939468+t-watanabe-seiei@users.noreply.github.com>
2024-07-14 06:12:14 +0000

    test8

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ AFRAME.registerComponent("vr-controller", {
     const text = document.getElementById("my_text");
     text.setAttribute("value", "Controller is ready!?");
 
-    this.el.addEventListener("gripdown", function(e) {
-      text.setAttribute("value", "GripDown!!");
-    });
-    this.el.addEventListener("gripup", function(e) {
-      text.setAttribute("value", "GripUp!!");
-    });
+    // this.el.addEventListener("gripdown", function(e) {
+    //   text.setAttribute("value", "GripDown!!");
+    // });
+    // this.el.addEventListener("gripup", function(e) {
+    //   text.setAttribute("value", "GripUp!!");
+    // });
 
-    this.el.addEventListener("triggerdown", function(e) {
-      text.setAttribute("value", "TriggerDown!!");
-    });
-    this.el.addEventListener("triggerup", function(e) {
-      text.setAttribute("value", "TriggerUp!!");
-    });
+    // this.el.addEventListener("triggerdown", function(e) {
+    //   text.setAttribute("value", "TriggerDown!!");
+    // });
+    // this.el.addEventListener("triggerup", function(e) {
+    //   text.setAttribute("value", "TriggerUp!!");
+    // });
 
     this.el.addEventListener("raycaster-intersection", function(e) {
       text.setAttribute("value", "Intersection:captured");
@@ -28,17 +28,17 @@ AFRAME.registerComponent("vr-controller", {
       text.setAttribute("value", "Intersection:cleared");
     });
 
-    // this.el.addEventListener("raycaster-intersected", function(e) {
-    //   text.setAttribute("value", "raycaster-intersected");
-    // });
+    this.el.addEventListener("raycaster-intersected", function(e) {
+      text.setAttribute("value", "raycaster-intersected");
+    });
 
-    // this.el.addEventListener("raycaster-intersected-cleared", function(e) {
-    //   text.setAttribute("value", "raycaster-intersected-cleared");
-    // });
+    this.el.addEventListener("raycaster-intersected-cleared", function(e) {
+      text.setAttribute("value", "raycaster-intersected-cleared");
+    });
     
-    // this.el.addEventListener("raycaster-closest-entity-changed", function(e) {
-    //   text.setAttribute("value", "raycaster-closest-entity-changed");
-    // });
+    this.el.addEventListener("raycaster-closest-entity-changed", function(e) {
+      text.setAttribute("value", "raycaster-closest-entity-changed");
+    });
 
     }
 });
